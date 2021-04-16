d84d63a41e317c644f56df53375bac55b7703b49
Pavle-Lakic <pavle.lakic994@gmail.com>
2021-04-16 17:48:26 +0200

    add function to generate random number

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,10 +2,25 @@
  *  @brief
  *  
  *  This file contains relevant defines, structures and
- *  functions that node.ino will call.
+ *  functions that main.cpp will call.
  *  
  *  @author Pavle Lakic
  *  @bug No known bugs
  */
 
 #include "includes.h"
+
+float random_number(void)
+{
+  float a;
+
+  a = ESP8266TrueRandom.random(10000);
+  a = a/10000;
+
+#if DEBUG
+  Serial.print("random_number = ");
+  Serial.println(a, 3);
+#endif
+
+  return a;
+}
